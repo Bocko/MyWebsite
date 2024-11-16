29f22c88f432b422819c571038d518c86c696b60
Botond Laszlo <lgbotond@gmail.com>
2024-11-16 10:26:59 +0100

    branch clearing for gh-pages

--- src/app/interfaces/repo-entry.ts
+++
@@ -1,8 +0,0 @@
-export interface RepoEntry {
-    name: string;
-    html_url: string;
-    description: string;
-    topics: string[];
-    forks: number;
-    stargazers_count: number;
-}
--- src/app/interfaces/timeline-entry.ts
+++
@@ -1,7 +0,0 @@
-export interface TimelineEntry {
-    logoPath: string;
-    title: string;
-    companyName: string;
-    periodStart: string;
-    periodEnd: string;
-}
